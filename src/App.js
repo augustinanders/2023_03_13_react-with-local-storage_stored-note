663a8a84655493166b9d6e05ab00b112394e6edc
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-03-13 17:09:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import FontSelector from "./components/FontSelector";
+import Note from "./components/Note";
+import "./styles.css";
+import useLocalStorageState from "use-local-storage-state";
+
+export default function App() {
+  const [note, setNote] = useLocalStorageState("note", { defaultValue: "" });
+  const [font, setFont] = useLocalStorageState("font", {
+    defaultValue: "system-ui",
+  });
+
+  function handleNoteChange(newNote) {
+    setNote(newNote);
+  }
+
+  function handleFontChange(newFont) {
+    setFont(newFont);
+  }
+
+  return (
+    <div className="app" style={{ "--font": font }}>
+      <FontSelector font={font} onFontChange={handleFontChange} />
+      <Note note={note} onNoteChange={handleNoteChange} />
+    </div>
+  );
+}
